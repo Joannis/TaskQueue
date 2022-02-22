6e7fd0a4602830c894013eda4ca811abdd8a62c9
Robbert Brandsma <i@robbertbrandsma.nl>
2022-02-22 18:20:25 +0100

    Added docs, helpers, and non-throwing versions

--- Sources/TaskQueue/TaskQueue.swift
+++ Sources/TaskQueue/TaskQueue.swift
@@ -1,15 +1,20 @@
+/// A queue that executes queued tasks serially, in the order they are queued.
 public final actor TaskQueue {
     private var task = Task<Void, Never> { }
 
+    /// Creates a new task group.
     public init() {}
     
+    /// Queue the given `work` in the task queue, waiting for any previously queued units of work to finish before executing the `work.`
+    ///
+    /// - Returns: The task representing the asynchronous `work`. The execution of `work` can be prevented by cancelling the returned task before the `work` is executed.
     @discardableResult
-    public func queueAction<T>(_ perform: @Sendable @escaping () async throws -> T) -> Task<T, Error> {
+    public func queueThrowingAction<T>(_ work: @Sendable @escaping () async throws -> T) -> Task<T, Error> {
         let currentTask = self.task
         let newTask = Task<T, Error> {
             await currentTask.value
             try Task.checkCancellation()
-            return try await perform()
+            return try await work()
         }
         
         self.task = Task {
@@ -17,5 +22,37 @@ public final actor TaskQueue {
         }
         
         return newTask
+    }
+    
+    /// Queue the given `work` in the task queue, waiting for any previously queued units of work to finish before executing the `work.`
+    ///
+    /// - Returns: The task representing the asynchronous `work`.
+    @discardableResult
+    public func queueAction<T>(_ work: @Sendable @escaping () async -> T) -> Task<T, Never> {
+        let currentTask = self.task
+        let newTask = Task<T, Never> {
+            await currentTask.value
+            return await work()
+        }
+        
+        self.task = Task {
+            _ = await newTask.value
+        }
+        
+        return newTask
+    }
+    
+    /// Run the given `work` in the task queue, waiting for any previously queued units of work to finish before executing the `work.`
+    ///
+    /// - Returns: The return value of `work`.
+    public func run<T>(_ work: @Sendable @escaping () async -> T) async -> T {
+        await queueAction(work).value
+    }
+    
+    /// Run the given `work` in the task queue, waiting for any previously queued units of work to finish before executing the `work.`
+    /// 
+    /// - Returns: The return value of `work`.
+    public func runThrowing<T>(_ work: @Sendable @escaping () async throws -> T) async throws -> T {
+        try await queueThrowingAction(work).value
     }
 }
